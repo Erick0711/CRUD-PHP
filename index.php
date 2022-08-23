c1d835f1a38d297ad859972f1e4214f75033bc03
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2022-08-23 17:37:38 -0400

    SE MODIFICO LA CARPETA CONTROLLER MODELS Y SE LOS COLOCO DENTRO DE LA CARPETA APP TAMBIEN SE CREO LOS ARCHIVOS PARA EDITAR REGISTRAR Y MOSTRAR LOS DATOS DE LA BASE DE DATOS

--- index.php
+++ index.php
@@ -1,37 +1,65 @@
+<?php require($_SERVER['DOCUMENT_ROOT']."/CRUD-php/App/Data/mostrar.php"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD</title>
     <link rel="icon" href="public/img/crud.png">
+    <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
+    <!-- ESTILOS CSS -->
+    <link rel="stylesheet" href="./public/css/style.css">
+    <!-- BOXICON -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
 
-    <div class="container border vh-100 d-flex justify-content-center align-items-center">
-        <table class="table table-primary table-bordered">
-            <thead>
+    <div class="container d-flex flex-column w-100 vh-100 d-flex justify-content-center align-items-center">
+        <h1 class="text-center">REGISTROS DE USUARIOS</h1>
+        <a href="./registro.php"><i class='bx bxs-user-plus bx-lg'></i></a>
+        <table class="table text-center table-bordered mt-4" id="tblDatos">
+            <thead class="table-primary">
                 <tr>
                     <th>ID</th>
+                    <th>CODIGO</th>
                     <th>NOMBRE</th>
-                    <th>3</th>
+                    <th>APELLIDO</th>
+                    <th>PAIS</th>
+                    <th>NIVEL DE EQUIPO</th>
+                    <th>CORREO</th>
+                    <th>FUNCIONES</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="table-dark">
+                <?php foreach($mostrarTodos as $mostrar){
+                    if($mostrar['estado'] == 1){
+                    ?>
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td><?php echo $mostrar['id'];?></td>
+                    <td><?php echo $mostrar['codigo'];?></td>
+                    <td><?php echo $mostrar['nombre'];?></td>
+                    <td><?php echo $mostrar['apellido'];?></td>
+                    <td><?php echo $mostrar['pais'];?></td>
+                    <td><?php echo $mostrar['nivel_de_equipo'];?></td>
+                    <td><?php echo $mostrar['correo'];?></td>
+                    <td>
+                    <a href="./edit.php?edit=<?php echo $mostrar['id']?>" name="edit" class="btn btn-warning" ><i class='bx bxs-pencil'></i></a>
+
+                        <a href="./App/Data/delete.php?borrar=<?php echo $mostrar['id']?>" class="btn btn-danger" name="borrar" id="borrar"><i class='bx bxs-trash-alt'></i></a>
+                    </td>
                 </tr>
+            <?php }
+        };?>
             </tbody>
         </table>
         
-
+        <div class="container-fluid d-flex justify-content-center align-item-center w-100">
+            <div class="" id="paginador"></div>
+        </div>
     </div>
 
 
@@ -39,11 +67,14 @@
 
 
 
-
+     <!-- SCRIPT BOOTSTRAP -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
-
+    <!-- SCRIPT PAGINADOR -->
+    <script src="./public/js/script.js"></script>
+    <!-- SCRIPT DE BUSCADOR -->
+    <script src="./public/js/search.js"></script>
 </body>
 
 </html>
---
+++ registro.php
@@ -0,0 +1,70 @@
+
+<?php require($_SERVER['DOCUMENT_ROOT']."/CRUD-php/App/Data/guardar.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <link rel="icon" href="public/img/crud.png">
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <!-- ESTILOS CSS -->
+    <link rel="stylesheet" href="./public/css/style.css">
+    <!-- BOXICON -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <div class="container d-flex flex-column w-80 vh-100 d-flex justify-content-center align-items-center">
+        <div class="container-editar">
+   <form action="./registro.php" autocomplete="off" method="POST">
+                    <h2 class="text-center text-white">Registrar Usuario</h2> 
+                    <div class="row mt-4">
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" placeholder="Nombre" name="nombre">
+                        </div>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" placeholder="Apellido" name="apellido">
+                        </div>
+                    </div>
+                    
+                    <div class="row mt-2">
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" placeholder="pais" name="pais">
+                        </div>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" placeholder="Nivel de equipo" name="nivel_equipo">
+                        </div>
+                        </div>
+
+                        <div class="row mt-2">
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" placeholder="Codigo" name="codigo">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" placeholder="Correo" name="correo">
+                            </div>
+                        </div>
+
+                        <div class="row mt-4">
+                            <div class="col-md-12 text-center">
+                                <input type="submit" name="guardar" class="btn btn-primary btn-block" value="Guardar">
+                            </div>
+                        </div>
+                </form>
+                </div>
+                </div>
+     <!-- SCRIPT BOOTSTRAP -->
+     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    <!-- SCRIPT PAGINADOR -->
+    <script src="./public/js/script.js"></script>
+    <!-- SCRIPT DE BUSCADOR -->
+    <script src="./public/js/search.js"></script>
+</body>
+
+</html>
